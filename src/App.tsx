8c0037c955ce48b2e879d30beebc82b827963b4d
ty ches <ttches@gmail.com>
2025-02-18 13:51:56 -0700

    added roadmap

--- src/App.tsx
+++ src/App.tsx
@@ -10,3 +10,11 @@ function App() {
 }
 
 export default App;
+
+/**
+ * Show the clipboard text above button
+ *  - it can be clicked to copy
+ * show more button lists previous acquisition names
+ *  - clicking on them will copy to clipboard
+ * Background flashes green when successfull, then dims
+ */
